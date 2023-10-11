58a764739b4bd2739c233fca8a3ade48c90dd1ce
dbalucas <lucas.duerksen.work@gmail.com>
2023-10-11 10:45:57 +0200

    create project resources and structure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dbalucas/Go-CRM-Backend_Udacity
 
 go 1.21.2
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,10 +1,93 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
+
+// Customer struct for the database model
+type Customer struct {
+	Name      string `json:"name,omitempty"`
+	Role      string `json:"role,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Phone     string `json:"phone,omitempty"`
+	Contacted bool   `json:"contacted,omitempty"`
+}
+
+// Mock Database and init values
+var Customers = map[uint32]Customer{
+	1: {
+		Name:  "Bernd",
+		Role:  "Sales",
+		Email: "sales.bernd@gmail.com",
+		Phone: "+49 876 54321",
+	},
+	2: {
+		Name:  "Bernd",
+		Role:  "Sales",
+		Email: "sales.bernd@gmail.com",
+		Phone: "+49 876 54321",
+	},
+	3: {
+		Name:  "Bernd",
+		Role:  "Sales",
+		Email: "sales.bernd@gmail.com",
+		Phone: "+49 876 54321",
+	},
+	4: {
+		Name:  "Bernd",
+		Role:  "Sales",
+		Email: "sales.bernd@gmail.com",
+		Phone: "+49 876 54321",
+	},
+	5: {
+		Name:  "Bernd",
+		Role:  "Sales",
+		Email: "sales.bernd@gmail.com",
+		Phone: "+49 876 54321",
+	},
+}
+
+// TODO:
+// brief overview of the API
+func serverStatic(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/html")
+	http.ServeFile(w, r, "static/index.html")
+	w.WriteHeader(http.StatusOK)
+}
+
+// Recieve all customers
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Context-Type", "application/json")
+	json.NewEncoder(w).Encode(Customers)
+	w.WriteHeader(http.StatusOK)
+}
+func addCustomer(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusCreated)
+}
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusOK)
+}
+func updateCustomer(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusOK)
+}
+func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+	// if not found fail
+	if false {
+
+		w.WriteHeader(http.StatusNotFound)
+	} else {
+
+		w.WriteHeader(http.StatusOK)
+	}
+}
 
 func webServe() {
 	const port = 3000
@@ -12,8 +95,17 @@ func webServe() {
 
 	var url = host + ":" + strconv.Itoa(port)
 
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", serverStatic)
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers/{id}", updateCustomer).Methods("PUT")
+	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
+
 	fmt.Println("Starting the server on: ", url)
-	http.ListenAndServe(url, nil)
+	http.ListenAndServe(url, router)
 }
 
 func main() {
