583c8742bcd01a4af914c06b10dd4851a019a3e5
dbalucas <lucas.duerksen.work@gmail.com>
2023-10-13 08:34:28 +0200

    reworked db structure and all funcHandlers

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/dbalucas/Go-CRM-Backend_Udacity
 go 1.21.2
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/google/uuid v1.3.1 // indirect
--- main.go
+++ main.go
@@ -3,124 +3,115 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"strconv"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
 // Customer struct for the database model
 type Customer struct {
-	Name      string `json:"name,omitempty"`
-	Role      string `json:"role,omitempty"`
-	Email     string `json:"email,omitempty"`
-	Phone     string `json:"phone,omitempty"`
-	Contacted bool   `json:"contacted,omitempty"`
+	ID        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Role      string    `json:"role,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Phone     int       `json:"phone,omitempty"`
+	Contacted bool      `json:"contacted,omitempty"`
 }
 
-// Mock Database and init values
-var Customers = map[string]Customer{
-	"1": {
-		Name:  "Anton",
-		Role:  "Sales",
-		Email: "sales.Anton@gmail.com",
-		Phone: "+49 876 54321",
-	},
-	"2": {
-		Name:  "Bernd",
-		Role:  "Sales",
-		Email: "sales.bernd@gmail.com",
-		Phone: "+49 876 54321",
-	},
-	"3": {
-		Name:  "Cäsar",
-		Role:  "Sales",
-		Email: "sales.caesar@gmail.com",
-		Phone: "+49 876 54321",
-	},
-	"4": {
-		Name:  "Doris",
-		Role:  "Sales",
-		Email: "sales.doris@gmail.com",
-		Phone: "+49 876 54321",
-	},
-	"5": {
-		Name:  "Emil",
-		Role:  "Sales",
-		Email: "sales.Emil@gmail.com",
-		Phone: "+49 876 54321",
-	},
+var mock_db []Customer
+var CurrentID uuid.UUID
+
+func init_db() {
+	mock_db = append(mock_db, Customer{ID: getNextUniqueID(), Name: "Anton", Role: "account manager", Email: "sales.Anton@gmail.com", Phone: 4987654321, Contacted: true})
+	mock_db = append(mock_db, Customer{ID: getNextUniqueID(), Name: "Bernd", Role: "marketing", Email: "marketing.bernd@gmail.com", Phone: 4987654321, Contacted: true})
+	mock_db = append(mock_db, Customer{ID: getNextUniqueID(), Name: "Cäsar", Role: "product manager", Email: "product.caesar@web.com", Phone: 4987654321, Contacted: true})
+	mock_db = append(mock_db, Customer{ID: getNextUniqueID(), Name: "Doris", Role: "admin", Email: "admin.doris@gmail.com", Phone: 4987654321, Contacted: true})
+	mock_db = append(mock_db, Customer{ID: getNextUniqueID(), Name: "Emil", Role: "Evangelist", Email: "Evangelist.Emil@gmail.com", Phone: 4987654321, Contacted: true})
+	fmt.Printf("Database initialized with %v entities. \n", len(mock_db))
+}
+
+func getNextUniqueID() uuid.UUID {
+	CurrentID := uuid.New()
+	return CurrentID
 }
 
 // TODO:
 // brief overview of the API
 func serverStatic(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/html")
+	w.Header().Set("Content-Type", "application/html")
 	http.ServeFile(w, r, "static/index.html")
-	w.WriteHeader(http.StatusOK)
 }
 
 // Recieve all customers
 func getCustomers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/json")
-	json.NewEncoder(w).Encode(Customers)
-	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(mock_db)
 }
 
 // From POST request create new Customer if not exists
 func addCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/json")
 	// new Entry var
 	var newCustomer Customer
-	// read the request body
-	requestBody, _ := io.ReadAll(r.Body)
-	// parse JSON body
-	json.Unmarshal(requestBody, &newCustomer)
+	// read the request body & parse JSON body
+	err := json.NewDecoder(r.Body).Decode(&newCustomer)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return // early  fail to avoid duplicate writing of status code
+	}
 	// insert new Entry into Customers map
-	// for k, v := range newCustomer {
-	// 	if _, ok := Customers[k]; !ok {
-	// 		Customers[k] = v
-	// 	}
+	newCustomer.ID = getNextUniqueID()
+	mock_db = append(mock_db, newCustomer)
 
-	// }
-
+	// output new entry
 	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newCustomer)
 }
 
 // Retrieve single customer by ID in /customer/{id}
 func getCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/json")
-	id := mux.Vars(r)["id"]
-	if _, ok := Customers[id]; ok {
-		json.NewEncoder(w).Encode(Customers[id])
-		w.WriteHeader(http.StatusOK)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
+	vars := mux.Vars(r)
+	CustomerID, _ := uuid.Parse(vars["id"])
+
+	var foundCustomer *Customer
+	for _, customer := range mock_db {
+		if customer.ID == CustomerID {
+			foundCustomer = &customer
+			break
+		}
 	}
+
+	if foundCustomer == nil {
+		http.Error(w, "Customer not found", http.StatusNotFound)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(foundCustomer)
 }
 
 func updateCustomer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Context-Type", "application/json")
 
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 }
+
 func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 	// if not found fail
 	if false {
-
 		w.WriteHeader(http.StatusNotFound)
 	} else {
-
 		w.WriteHeader(http.StatusOK)
 	}
 }
 
-func webServe() {
+func main() {
 	const port = 3000
 	const host = "localhost"
 
 	var url = host + ":" + strconv.Itoa(port)
+
+	init_db()
 
 	router := mux.NewRouter()
 
@@ -136,9 +127,4 @@ func webServe() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func main() {
-
-	webServe()
 }
