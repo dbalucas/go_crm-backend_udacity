dccc2cbbd6f1742a2cc0acba0a79a6c13127295e
dbalucas <lucas.duerksen.work@gmail.com>
2023-10-18 16:36:56 +0200

    exclude database and make database none global

--- main.go
+++ main.go
@@ -21,6 +21,11 @@ type Customer struct {
 	Contacted bool      `json:"contacted,omitempty"`
 }
 
+// Repository alice of all Customers
+type CustomerRepository struct {
+	customers []Customer
+}
+
 // type CustomerRepository interface {
 // 	// Save
 // 	FindAll() ([]Customer, error)
@@ -29,17 +34,13 @@ type Customer struct {
 // 	DeleteByID(uuid.UUID) error
 // }
 
-// var mockDb []Customer
-
-type CustomerRepository struct {
-	customers []Customer
-}
-
-func (c CustomerRepository) FindAll() ([]Customer, error) {
+// retrieve all customers from database
+func (c *CustomerRepository) FindAll() ([]Customer, error) {
 	return c.customers, nil
 }
 
-func (c CustomerRepository) findIndexByID(id uuid.UUID) (int, error) {
+// retrieve the database index by uuid
+func (c *CustomerRepository) findIndexByID(id uuid.UUID) (int, error) {
 	for index, customer := range c.customers {
 		if customer.ID == id {
 			return index, nil
@@ -48,7 +49,8 @@ func (c CustomerRepository) findIndexByID(id uuid.UUID) (int, error) {
 	return 0, errors.New("not found")
 }
 
-func (c CustomerRepository) FindByID(id uuid.UUID) (Customer, error) {
+// find a user by uuid in database if exists
+func (c *CustomerRepository) FindByID(id uuid.UUID) (Customer, error) {
 	index, err := c.findIndexByID(id)
 	if err != nil {
 		return c.customers[index], nil
@@ -56,30 +58,39 @@ func (c CustomerRepository) FindByID(id uuid.UUID) (Customer, error) {
 	return Customer{}, errors.New("not found")
 }
 
-func (c CustomerRepository) Save(newCustomer Customer) (Customer, error) {
+// Save newCustomer to Database (UUID generated automatically)
+func (c *CustomerRepository) Save(newCustomer Customer) (Customer, error) {
+	// create new ID if not exists
+	if newCustomer.ID == uuid.Nil {
+		newCustomer.ID = getNextUniqueID()
+	}
 	c.customers = append(c.customers, newCustomer)
 	return newCustomer, nil
 }
 
-func (c CustomerRepository) DeleteByID(id uuid.UUID) error {
+// delete a customer by uuid from database
+func (c *CustomerRepository) DeleteByID(id uuid.UUID) error {
 	index, err := c.findIndexByID(id)
 	if err != nil {
-		c.customers = append(c.customers[:index], c.customers[index+1:]...)
-		return nil
+		return errors.New("can not delete by ID: " + id.String() + " not found")
 	}
-	return errors.New("not found")
+	c.customers = deleteAtIndex(c.customers, index)
+	return nil
 
 }
 
-var CurrentID uuid.UUID
+// from inserted slice, remove item by slice index
+func deleteAtIndex(slice []Customer, index int) []Customer {
+	return append(slice[:index], slice[index+1:]...)
+}
 
-func init_db(customerRepo CustomerRepository) {
-	customerRepo.Save(Customer{ID: getNextUniqueID(), Name: "Anton", Role: "account manager", Email: "sales.Anton@gmail.com", Phone: 4987654321, Contacted: true})
-	customerRepo.Save(Customer{ID: getNextUniqueID(), Name: "Bernd", Role: "marketing", Email: "marketing.bernd@gmail.com", Phone: 4987654321, Contacted: true})
-	customerRepo.Save(Customer{ID: getNextUniqueID(), Name: "Cäsar", Role: "product manager", Email: "product.caesar@web.com", Phone: 4987654321, Contacted: true})
-	customerRepo.Save(Customer{ID: getNextUniqueID(), Name: "Doris", Role: "admin", Email: "admin.doris@gmail.com", Phone: 4987654321, Contacted: true})
-	customerRepo.Save(Customer{ID: getNextUniqueID(), Name: "Emil", Role: "Evangelist", Email: "Evangelist.Emil@gmail.com", Phone: 4987654321, Contacted: true})
-	fmt.Printf("Database initialized with %v entities. \n", len(customerRepo.customers))
+func init_db(customerRepository *CustomerRepository) {
+	customerRepository.Save(Customer{Name: "Anton", Role: "account manager", Email: "sales.Anton@gmail.com", Phone: 4987654321, Contacted: true})
+	customerRepository.Save(Customer{Name: "Bernd", Role: "marketing", Email: "marketing.bernd@gmail.com", Phone: 4987654321, Contacted: true})
+	customerRepository.Save(Customer{Name: "Cäsar", Role: "product manager", Email: "product.caesar@web.com", Phone: 4987654321, Contacted: true})
+	customerRepository.Save(Customer{Name: "Doris", Role: "admin", Email: "admin.doris@gmail.com", Phone: 4987654321, Contacted: true})
+	customerRepository.Save(Customer{Name: "Emil", Role: "Evangelist", Email: "Evangelist.Emil@gmail.com", Phone: 4987654321, Contacted: true})
+	fmt.Printf("Database initialized with %v entities. \n", len(customerRepository.customers))
 }
 
 func getNextUniqueID() uuid.UUID {
@@ -95,14 +106,14 @@ func serverStatic(w http.ResponseWriter, r *http.Request) {
 }
 
 // Recieve all customers
-func getCustomers(w http.ResponseWriter, r *http.Request) {
+func getCustomers(w http.ResponseWriter, r *http.Request, c *CustomerRepository) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(mockDb)
+	json.NewEncoder(w).Encode(c.customers)
 	// add pagination
 }
 
 // From POST request create new Customer if not exists
-func addCustomer(w http.ResponseWriter, r *http.Request) {
+func addCustomer(w http.ResponseWriter, r *http.Request, c *CustomerRepository) {
 	// new Entry var
 	var newCustomer Customer
 	// read the request body & parse JSON body
@@ -112,79 +123,80 @@ func addCustomer(w http.ResponseWriter, r *http.Request) {
 		return // early  fail to avoid duplicate writing of status code
 	}
 
-	// auslagern in eigenständige Funktion
 	// insert new Entry into Customers map
-	newCustomer.ID = getNextUniqueID()
-	mockDb = append(mockDb, newCustomer)
+	c.Save(newCustomer)
 
+	// get new entry from database for validation pupose
+	customer, err := c.FindByID(newCustomer.ID)
+	if err != nil {
+		errors.New("could retrieve newly added item from database" + newCustomer.ID.String())
+		w.WriteHeader(http.StatusNotFound)
+	}
 	// output new entry
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(newCustomer)
+	json.NewEncoder(w).Encode(customer)
 }
 
 // Retrieve single customer by ID in /customer/{id}
 func getCustomer(w http.ResponseWriter, r *http.Request, c *CustomerRepository) {
 	vars := mux.Vars(r)
 	CustomerID, _ := uuid.Parse(vars["id"])
+	foundCustomer, err := c.FindByID(CustomerID)
 
-	var foundCustomer *Customer
-	for _, customer := range mockDb {
-		if customer.ID == CustomerID {
-			foundCustomer = &customer
-			break
-		}
-	}
-
-	if foundCustomer == nil {
+	if err != nil {
 		http.Error(w, "Customer not found", http.StatusNotFound)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(foundCustomer)
 }
 
-func updateCustomer(w http.ResponseWriter, r *http.Request) {
+// update Customer by ID
+func updateCustomer(w http.ResponseWriter, r *http.Request, c *CustomerRepository) {
+	vars := mux.Vars(r)
+	CustomerID, _ := uuid.Parse(vars["id"])
 
+	// new Entry var
+	var updateCustomer Customer
+	// read the request body & parse JSON body
+	err := json.NewDecoder(r.Body).Decode(&updateCustomer)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return // early  fail to avoid duplicate writing of status code
+	}
+	updateCustomer.ID = CustomerID
+	// insert new Entry into Customers map
+
+	updatedCustomer, err := c.Save(updateCustomer)
+	if err != nil {
+		http.Error(w, "Customer not found: "+updatedCustomer.ID.String(), http.StatusNotFound)
+		w.WriteHeader(http.StatusNotFound)
+	}
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 }
 
-func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+// remove a single Customer by ID
+func deleteCustomer(w http.ResponseWriter, r *http.Request, c *CustomerRepository) {
 	// Get ID from Var ULR
 	vars := mux.Vars(r)
 	customerID, _ := uuid.Parse(vars["id"])
-	var foundCustomer *Customer
-	var mockDbIndex int
 
-	for sliceIndex, customer := range mockDb {
-		if customer.ID == customerID {
-			foundCustomer = &customer
-			mockDbIndex = sliceIndex
-			break
-		} else {
-			// do nothing
-			foundCustomer = nil
-		}
-	}
+	err := c.DeleteByID(customerID)
 	// if not found fail else remove
-	if foundCustomer == nil {
+	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 	} else {
 		w.WriteHeader(http.StatusOK)
-		mockDb = deleteAt(mockDb, mockDbIndex)
-		fmt.Fprintf(w, "Custemer %v %v was removed", foundCustomer.ID, foundCustomer.Name)
+		fmt.Fprintf(w, "Customer %v was removed", customerID)
 	}
 }
 
-func deleteAt(slice []Customer, index int) []Customer {
-	return append(slice[:index], slice[index+1:]...)
-}
+// better readability of wrapper function
+type CustomHandler func(http.ResponseWriter, *http.Request, *CustomerRepository)
 
-type MuxHandler = func(http.ResponseWriter, *http.Request)
-
-type CustomHandler = func(http.ResponseWriter, *http.Request, *CustomerRepository)
-
-func wrap(f CustomHandler, c *CustomerRepository) MuxHandler {
+// wrap HandleFunc to handover CustomerRepository Pointer into called function like getCustomers,...
+func wrap(f CustomHandler, c *CustomerRepository) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		f(w, r, c)
 	}
@@ -203,11 +215,11 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", serverStatic)
-	router.HandleFunc("/customers", wrap(getCustomer, customerRepository)).Methods("GET")
-	router.HandleFunc("/customers", addCustomer).Methods("POST")
-	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
-	router.HandleFunc("/customers/{id}", updateCustomer).Methods("PUT")
-	router.HandleFunc("/customers/{id}", deleteCustomer).Methods("DELETE")
+	router.HandleFunc("/customers", wrap(getCustomers, customerRepository)).Methods("GET")
+	router.HandleFunc("/customers", wrap(addCustomer, customerRepository)).Methods("POST")
+	router.HandleFunc("/customers/{id}", wrap(getCustomer, customerRepository)).Methods("GET")
+	router.HandleFunc("/customers/{id}", wrap(updateCustomer, customerRepository)).Methods("PUT")
+	router.HandleFunc("/customers/{id}", wrap(deleteCustomer, customerRepository)).Methods("DELETE")
 
 	fmt.Println("Starting the server on: ", url)
 	err := http.ListenAndServe(url, router)
